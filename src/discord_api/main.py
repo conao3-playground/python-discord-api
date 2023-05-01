07ad4553cfee189e7224bf2356b1d45193b6c4ab
Naoya Yamashita <conao3@gmail.com>
2023-05-01 14:11:24 +0900

    implement first bot

--- src/discord_api/main.py
+++ src/discord_api/main.py
@@ -1,2 +1,31 @@
+import discord
+import pydantic
+
+class Settings(pydantic.BaseSettings):
+    bot_token: str
+
+    class Config:  # type: ignore
+        env_file = '.env'
+
+settings = Settings()  # type: ignore
+
+client = discord.Client(intents=discord.Intents.all())
+
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+
+@client.event
+async def on_message(message: discord.Message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+
 def main():
     print('Hello, world!')
+    client.run(settings.bot_token)
